0c6902e1cd225f14938544f4002182b7b48108eb
shay5555 <62674745+shay5555@users.noreply.github.com>
2022-12-26 02:50:37 +0200

    Add basic color on hover functionality

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const sketchContainer = document.querySelector('#sketch-container');
 
-function createDiv(divTotal=16*16) {
+//create sketch grid and assign class name to all of them
+(function createGrid(divTotal=16*16) {
     for (let i=0; i<divTotal; i++) {
         const newDiv = document.createElement('div');
+        newDiv.classList.add('color-on-hover');
         sketchContainer.appendChild(newDiv);
     }
-}
+})();
 
-createDiv();
+//get every square on the grid
+const colorOnHover = document.getElementById('sketch-container').getElementsByClassName('color-on-hover');
+
+//add event listener to every div of a square that activates on hover
+for (let i=0; i<colorOnHover.length; i++) {
+    colorOnHover[i].addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'blue';
+    });
+}
