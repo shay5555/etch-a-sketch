1ce828678b7301b5b4f007db512a49913c71d7a6
shay5555 <62674745+shay5555@users.noreply.github.com>
2022-12-26 04:11:36 +0200

    Add grid size changer set by user input

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 const sketchContainer = document.querySelector('#sketch-container');
 
 //create sketch grid and assign class name to all of them
-(function createGrid(divTotal=16*16) {
+const createGridReference = (function createGrid(divTotal=16) {
+    sketchContainer.style.gridTemplateColumns = `repeat(${divTotal}, 1fr)`;
+    sketchContainer.style.gridTemplateRows = `repeat(${divTotal}, 1fr)`;
+    divTotal *= divTotal;
+
     for (let i=0; i<divTotal; i++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add('color-on-hover');
         sketchContainer.appendChild(newDiv);
     }
-})();
+    return createGrid;
+})(16);
 
-//get every square on the grid
+//get every square on the grid on colorOnHover
 const colorOnHover = document.getElementById('sketch-container').getElementsByClassName('color-on-hover');
 
 //add event listener to every div of a square that activates on hover
-for (let i=0; i<colorOnHover.length; i++) {
-    colorOnHover[i].addEventListener('mouseover', (e) => {
-        e.target.style.backgroundColor = 'blue';
-    });
+const colorGridReference = (function colorGrid () {
+    for (let i=0; i<colorOnHover.length; i++) {
+        colorOnHover[i].addEventListener('mouseover', (e) => {
+            e.target.style.backgroundColor = 'blue';
+        });
+    }
+    return colorGrid;
+})();
+
+//set grid size by using prompt from the user
+function setGridSize () {
+    sketchContainer.textContent = '';
+    let gridSize = prompt("Number of squares per side: ");
+    createGridReference(gridSize);
+    colorGridReference();
 }
